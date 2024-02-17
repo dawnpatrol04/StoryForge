b915c02951c42285225d2179beb8368f3ca8f197
dawnpatrol04 <dawnpatrol04@gmail.com>
2024-02-17 11:35:33 -0500

    updating for round 2 of dev work

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# from characters import MainCharacterChain
+from characters import MainCharacterChain
 from structure import get_structure
 from events import get_events
 from writing import write_book
@@ -12,7 +12,7 @@ subject = 'The RV surf trip of 2024'
 author='Bruce Brown'
 genre='Adventure'
 
-# main_character_chain = MainCharacterChain()
+main_character_chain = MainCharacterChain()
 doc_writer = DocWriter()
 
 title, plot, chapter_dict = get_structure(
@@ -20,6 +20,7 @@ title, plot, chapter_dict = get_structure(
     genre, 
     author
 )
+
 summaries_dict, event_dict = get_events(
     subject, 
     genre, 
--- characters.py
+++ characters.py
@@ -1,34 +1,34 @@
-# import os
-# # from langchain.document_loaders import PyPDFLoader
-# from langchain_community.document_loaders import PyPDFLoader
-# from langchain.chat_models import ChatOpenAI
-# from langchain.chains import LLMChain
+import os
+# from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 
-# class MainCharacterChain:
+class MainCharacterChain:
 
-#     PROMPT = """
-#     You are provided with the resume of a person. 
-#     Describe the person's profile in a few sentences and include that person's name.
+    PROMPT = """
+    You are provided with the resume of a person. 
+    Describe the person's profile in a few sentences and include that person's name.
 
-#     Resume: {text}
+    Resume: {text}
 
-#     Profile:"""
+    Profile:"""
 
-#     def __init__(self) -> None:
+    def __init__(self) -> None:
 
-#         self.llm = ChatOpenAI()
-#         self.chain = LLMChain.from_string(
-#             llm=self.llm,
-#             template=self.PROMPT
-#         )
+        self.llm = ChatOpenAI()
+        self.chain = LLMChain.from_string(
+            llm=self.llm,
+            template=self.PROMPT
+        )
 
-#         self.chain.verbose = True
+        self.chain.verbose = True
 
-#     def load_resume(self, file_name):
-#         folder = 'I am a cowboy and I like to ride on my horse'
-#         return folder
+    def load_resume(self, file_name):
+        folder = 'I am a cowboy and I like to ride on my horse'
+        return folder
 
-#     def run(self, file_name):
-#         docs = self.load_resume(file_name)
-#         resume = '\n\n'.join([doc.page_content for doc in docs])
-#         return self.chain.run(resume)
+    def run(self, file_name):
+        docs = self.load_resume(file_name)
+        resume = '\n\n'.join([doc.page_content for doc in docs])
+        return self.chain.run(resume)
